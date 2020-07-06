5384fcb7c93f78cda5ed3208466bf7f732b66d0a
Old <onelifedesigning@gmail.com>
2020-07-06 18:12:49 +0200

    iteration 2

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,19 @@
+// Iteration #1
+require('../configs/db.config')
+
+const mongoose = require('mongoose');
+const Celebrity = require('../models/celebrity');
+
+const celebrities = [
+  { name: 'Nelson Mandela', occupation: 'Lawyer', catchPhrase: 'The greatest glory in living lies not in never falling, but in rising every time we fall.' },
+  { name: 'Walt Disney', occupation: 'Film director', catchPhrase: 'The way to get started is to quit talking and begin doing.' },
+  { name: 'Steve Jobs', occupation: 'Businessman', catchPhrase: 'Your time is limited, so don´t waste it living someone else´s life. Don´t be trapped by dogma – which is living with the results of other people´s thinking' }
+];
+
+Celebrity.deleteMany({})
+  .then(Celebrity.insertMany(celebrities))
+  .then(() => {
+    console.log('Created registers correctly')
+    mongoose.connection.close()
+  })
+  .catch((err) => console.log(err))
